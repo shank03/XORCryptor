07c3cd7d10f018e57396f8d76cb24c579151ea2a
Shashank Verma <shashank.verma2002@gmail.com>
2021-05-05 11:56:52 +0800

    Update Copyright
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- JVM/com/beesechurger/shank03/XORCryptor.java
+++ JVM/com/beesechurger/shank03/XORCryptor.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2021, Shashank Verma <shashank.verma2002@gmail.com>(shank03)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
 package com.beesechurger.shank03;
 
 import java.util.Random;
@@ -7,8 +21,7 @@ import java.util.Random;
  * <p>
  * Encrypts input text using XOR operation with individual characters
  * from input, key and randomized generated character.
- *
- * @author Shashank Verma (shank03) <shashank.verma2002@gmail.com>
+ * <p>
  * date: 02-May-2021
  */
 public class XORCryptor {
--- Nodejs/lib/xor-crypt.ts
+++ Nodejs/lib/xor-crypt.ts
@@ -1,10 +1,23 @@
 /*
- * Cryptor Class
+ * Copyright (c) 2021, Shashank Verma <shashank.verma2002@gmail.com>(shank03)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+/**
+ * XORCryptor
  * <p>
  * Encrypts input text using XOR operation with individual characters
  * from input, key and randomized generated character.
  *
- * @author Shashank Verma (shank03) <shashank.verma2002@gmail.com>
  * date: 02-May-2021
  */
 
